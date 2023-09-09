a971ef6197dc9c3d917f750942e7d33e6a992f4e
Tunahan <47463551+devtunahan@users.noreply.github.com>
2023-09-09 22:13:59 +0200

    added / endoint for vercel

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cheerio = require('cheerio');
 const countryFlags = require('./countrys_flags');
 const app = express();
 
+app.get("/" ,async(req,res) => {
+  console.log("test")
+})
+
 // Endpoint for getting the IP Address
 app.get('/ip', async (req, res) => {
  axios.get('https://api.ipify.org?format=json')
